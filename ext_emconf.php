8d10de6bf2f6548b9c85915fc035391a0e3ae307
Claus Due <claus@wildside.dk>
2012-11-04 16:39:03 +0100

    [TER] 0.9.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,15 @@
 <?php
 
+########################################################################
+# Extension Manager/Repository config file for ext "extbase_realurl".
+#
+# Auto generated 04-11-2012 16:38
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Extbase Realurl: Automatic Realurl configuration for Extbase plugins',
 	'description' => 'Creates automatic Realurl rules for all plugins used on the TYPO3 site. Enables extension developers to use class and method annotations to modify the rules that are generated. Supports all versions of Extbase.',
@@ -19,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.9.0',
+	'version' => '0.9.1',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5-0.0.0',
@@ -34,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:20:"class.tx_realurl.php";s:4:"9691";s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"eb18";s:9:"README.md";s:4:"9b90";s:38:"Classes/AutoConfigurationGenerator.php";s:4:"dc9f";s:39:"Classes/RoutableControllerInterface.php";s:4:"ed1c";s:29:"Classes/RoutingAnnotation.php";s:4:"50ff";s:28:"Classes/RoutingException.php";s:4:"7ca3";s:33:"Classes/SegmentValueProcessor.php";s:4:"4a62";}',
+	'_md5_values_when_last_written' => 'a:9:{s:20:"class.tx_realurl.php";s:4:"9691";s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"eb18";s:9:"README.md";s:4:"9b90";s:38:"Classes/AutoConfigurationGenerator.php";s:4:"928b";s:39:"Classes/RoutableControllerInterface.php";s:4:"ed1c";s:29:"Classes/RoutingAnnotation.php";s:4:"50ff";s:28:"Classes/RoutingException.php";s:4:"7ca3";s:33:"Classes/SegmentValueProcessor.php";s:4:"4a62";}',
 );
 
 ?>
