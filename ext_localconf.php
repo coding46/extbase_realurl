d50d709116a45cfc48a0e1ef83a57c0017c3ff13
Claus Due <claus@wildside.dk>
2013-06-09 22:36:10 +0200

    [TASK] Remove a no longer required overridden RealUrl class

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,12 +4,3 @@ if (!defined('TYPO3_MODE')) {
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['extbase_realurl'] = 'Tx_ExtbaseRealurl_AutoConfigurationGenerator->buildAutomaticRules';
-
-$version = TYPO3_version;
-if ($version{0} >= 6) {
-	require_once t3lib_extMgm::extPath('extbase_realurl', 'class.tx_realurl.php');
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['tx_realurl'] =
-		array('className' => 'ux_tx_realurl');
-} else {
-	$GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/realurl/class.tx_realurl.php'] = t3lib_extMgm::extPath('extbase_realurl', 'class.tx_realurl.php');
-}
